b2e9160fe71761937380168e8628f2ac1f2dd276
ericjoji <ericjoji303@gmail.com>
2026-05-27 20:05:46 -0300

    feat(py3_mundo1_cev): criar arquivo de desafio 000 py3_mundo1

---
+++ aprendendo-python/python3_cev/dsf000.py
@@ -0,0 +1,5 @@
+# Criar um script que leia o nome de uma pessoa e mostra uma mensagem de boas-vindas de acordo com o valor digitado.
+
+# Crie um script que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+
+# Crie um script que leia dois números e tente mostrar a soma entre eles.
